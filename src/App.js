0185d3d30b7536ab20b3c21e14161c33db0d3b10
Raviteja Chilaka <chilakaraviteja@yahoo.com>
2023-03-03 22:15:41 +0000

    chore: restored to original files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from "./Components/NavBar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Switch>
-            <Route path="/">
-              <NavBar />
-            </Route>
-          </Switch>
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
