32a5879e4ddc95b0106c2b106e1ad782ab28d067
Raviteja Chilaka <chilakaraviteja@yahoo.com>
2023-03-04 14:20:18 +0000

    feature: added Nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const NavBar = () => {
+  const [isToggleBar, setIsToggleBar] = useState(false);
+  const Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICES", link: "/services" },
+    { name: "BLOG", link: "/blog" },
+    { name: "ABOUT ME", link: "/about-me" },
+  ];
+  return (
+    <div className="w-full fixed top-0 left-0 bg-blue-500 text-white">
+      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
+        <div className="text-xl cursor-pointer flex items-center font-[Merienda]">
+          Ravi Teja Chilaka
+        </div>
+        <div
+          onClick={() => setIsToggleBar(!isToggleBar)}
+          className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden"
+        >
+          <ion-icon
+            name={isToggleBar ? "close-outline" : "menu-outline"}
+          ></ion-icon>
+        </div>
+        <ul
+          className={`md:flex md:items-center pb-0 absolute md:static bg-blue-500 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-7 transition-all duration-500 ease-in-out ${
+            isToggleBar ? "top-10 text-white" : "top-[-400px]"
+          }`}
+        >
+          {Links.map(({ name, link }, index) => (
+            <li key={index} className="text-xs md:ml-8  md:my-0 my-5 ">
+              {" "}
+              <a
+                href={link}
+                className="hover:text-gray-900 duration-300 font-[Merienda]"
+              >
+                {name}{" "}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
