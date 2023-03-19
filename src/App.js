76b8905b43e31d70dd290d0bee082e71a2706172
Raviteja Chilaka <chilakaraviteja@yahoo.com>
2023-03-19 21:53:31 +0000

    fixed lint warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
 import NavBar from "./components/NavBar";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
